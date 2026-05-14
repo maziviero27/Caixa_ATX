e1def6372749784c39ee50112ab7c1df919429f2
TOMMASO NAGLIATTI <tommasonagliatti7@gmail.com>
2026-05-14 19:06:53 -0300

    Criei o gitignore e terminei o codigo

--- main.c
+++ main.c
@@ -87,6 +87,33 @@ int main() {
     char login_nome[20];
     char login_senha[20];
 
+    printf("Digite seu nome: ");
+    scanf("%s", login_nome);
+
+    printf("Digite sua senha: ");
+    scanf("%s", login_senha);
+
+    for (int i = 0; i < 2; i++) {
+
+        if (strcmp(data_base[i][0], login_nome) == 0 &&
+            strcmp(data_base[i][1], login_senha) == 0) {
+
+            printf("Bem vindo %s\n", data_base[i][0]);
+
+            encontrado = 1;
+        }
+    }
+
+    if (encontrado == 0) {
+
+        printf("Usuario ou senha incorretos.\n");
+
+    } else {
+
+        switch_case();
+    }
+
+    printf("Sessao Encerrada\n");
 
     return 0;
 }
