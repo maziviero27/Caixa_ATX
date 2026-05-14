c8045f8d1ee940d3af250e3a13beee30a0e9b2e2
Arthur Maziviero Faria <arthur_mfaria@hotmail.com>
2026-05-13 23:02:30 -0300

    Criando o switch case e incluindo a biblioteca 'string.h' para interpretacao de espacos

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 float deposito() {
 
@@ -25,4 +26,51 @@ float saque(float var1){
     else{
     return valor_out;
     }
+}
+
+void switch_case() {
+
+    float saldo = 0.00;
+    int escolha;
+
+    do {
+
+        printf("\n================================\n");
+        printf("SEU SALDO ATUAL E DE: R$ %.2f\n", saldo);
+        printf("================================\n");
+
+        printf("0 - Deposito\n");
+        printf("1 - Saque\n");
+        printf("2 - Encerrar programa\n");
+        printf("================================\n");
+
+        printf("Escolha uma opcao: ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+
+            case 0:
+
+                saldo += deposito();
+
+                break;
+
+            case 1:
+
+                saldo -= saque(saldo);
+
+                break;
+
+            case 2:
+
+                printf("Encerrando sessao...\n");
+
+                break;
+
+            default:
+
+                printf("Codigo invalido!\n");
+        }
+
+    } while (escolha != 2);
 }
